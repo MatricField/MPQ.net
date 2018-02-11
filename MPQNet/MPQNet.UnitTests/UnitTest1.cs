fb5a781c72f3acbfa50be1e4fba4aa0ad35c8b92
MatricField <david.xelnaga@gmail.com>
2018-02-10 18:41:11 -0800

    Added the rest of header entries.

--- MPQNet/MPQNet.UnitTests/UnitTest1.cs
+++ MPQNet/MPQNet.UnitTests/UnitTest1.cs
@@ -36,6 +36,12 @@ namespace MPQNet.UnitTests
                 case ArchiveHeaderV1 v1:
                     Assert.AreEqual(headerPY["magic"], v1.ID, nameof(ArchiveHeaderV1.ID));
                     Assert.AreEqual(headerPY["header_size"], v1.HeaderSize, nameof(ArchiveHeaderV1.HeaderSize));
+                    Assert.AreEqual(headerPY["format_version"], v1.FormatVersion, nameof(ArchiveHeaderV1.FormatVersion));
+                    Assert.AreEqual(headerPY["sector_size_shift"], v1.SectorSizeShift, nameof(ArchiveHeaderV1.SectorSizeShift));
+                    Assert.AreEqual(headerPY["hash_table_offset"], v1.HashTableOffset, nameof(ArchiveHeaderV1.HashTableOffset));
+                    Assert.AreEqual(headerPY["block_table_offset"], v1.BlockTableOffset, nameof(ArchiveHeaderV1.BlockTableOffset));
+                    Assert.AreEqual(headerPY["hash_table_entries"], v1.HashTableEntriesCount, nameof(ArchiveHeaderV1.HashTableEntriesCount));
+                    Assert.AreEqual(headerPY["block_table_entries"], v1.BlockTableEntriesCount, nameof(ArchiveHeaderV1.BlockTableEntriesCount));
                     break;
             }
         }
